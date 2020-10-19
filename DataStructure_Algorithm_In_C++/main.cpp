7e37598a68383c3f9bbd84989562ab30461391e6
Nguyen, Long <Long.Nguyen@F-Secure.com>
2020-10-19 21:27:25 +0300

    Initial Commit

---
+++ DataStructure_Algorithm_In_C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure_Algorithm_In_C++
+//
+//  Created by Nguyen, Long on 10/19/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
